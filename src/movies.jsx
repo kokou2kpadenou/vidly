1741b4f2506c656b35ca0704ee1d1c0e1291523f
kokou kpadenou <kkokou@gmail.com>
2019-06-05 23:18:38 -0400

    Pagination Component

--- src/movies.jsx
+++ src/movies.jsx
@@ -1,14 +1,22 @@
 import React, { Component } from "react";
 import { getMovies } from "./services/fakeMovieService";
 import Movie from "./components/movie";
+import Pagination from "./components/common/pagination";
+import { paginate } from "./utils/paginate";
 
 class Movies extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      movies: getMovies()
+      movies: getMovies(),
+      pageSize: 4,
+      currentPage: 1
     };
   }
+
+  HandlePageChange = page => {
+    this.setState({ currentPage: page });
+  };
 
   HandleLike = movieID => {
     this.setState(oldState => ({
@@ -35,34 +43,48 @@ class Movies extends Component {
         </div>
       );
 
+    const movies = paginate(
+      this.state.movies,
+      this.state.currentPage,
+      this.state.pageSize
+    );
+
     return (
       <div className="container">
         <p className="">{`Showing ${
           this.state.movies.length
         } movies in the database.`}</p>
         {this.state.movies.length > 0 && (
-          <table className="table">
-            <thead>
-              <tr>
-                <th>Title</th>
-                <th>Genre</th>
-                <th>Stock</th>
-                <th>Rate</th>
-                <th>&nbsp;</th>
-                <th>&nbsp;</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.state.movies.map(movie => (
-                <Movie
-                  key={movie._id}
-                  data={movie}
-                  onClick={this.handleClickDelete}
-                  onLike={this.HandleLike}
-                />
-              ))}
-            </tbody>
-          </table>
+          <>
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Title</th>
+                  <th>Genre</th>
+                  <th>Stock</th>
+                  <th>Rate</th>
+                  <th>&nbsp;</th>
+                  <th>&nbsp;</th>
+                </tr>
+              </thead>
+              <tbody>
+                {movies.map(movie => (
+                  <Movie
+                    key={movie._id}
+                    data={movie}
+                    onClick={this.handleClickDelete}
+                    onLike={this.HandleLike}
+                  />
+                ))}
+              </tbody>
+            </table>
+            <Pagination
+              itemsCount={this.state.movies.length}
+              pageSize={this.state.pageSize}
+              currentPage={this.state.currentPage}
+              onPageChange={this.HandlePageChange}
+            />
+          </>
         )}
       </div>
     );
