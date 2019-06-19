d864caafcd8a36e9792a92b5d6e43c01b56f262c
kokou kpadenou <kkokou@gmail.com>
2019-06-18 21:20:09 -0400

    register form

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -35,6 +35,10 @@ const NavBar = () => {
           <NavLink className="nav-item nav-link" to="/login">
             Login
           </NavLink>
+
+          <NavLink className="nav-item nav-link" to="/register">
+            Register
+          </NavLink>
         </div>
       </div>
     </nav>
---
+++ src/components/registerForm.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Joi from "joi-browser";
+import Form from "./common/form";
+
+class RegisterForm extends Form {
+  state = { data: { username: "", password: "", name: "" }, errors: {} };
+
+  schema = {
+    username: Joi.string()
+      .required()
+      .email({ minDomainAtoms: 2 })
+      .label("Username"),
+    password: Joi.string()
+      .required()
+      .min(5)
+      .label("Password"),
+    name: Joi.string()
+      .required()
+      .label("Name")
+  };
+
+  doSubmit = () => {
+    console.log("submited");
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Register</h2>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("username", "Username", undefined, true)}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderInput("name", "Name")}
+          {this.renderButton("Register")}
+        </form>
+      </div>
+    );
+  }
+}
+
+export default RegisterForm;
