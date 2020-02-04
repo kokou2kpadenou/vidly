b26027868d0a1a148e15bcd0399557646453ce2f
kokou kpadenou <kkokou@gmail.com>
2020-02-04 13:22:11 -0500

    extract layout for movies, customers and rental list pages

---
+++ src/components/layout.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StatusMessage from "./statusMessage";
+import Pagination from "./common/pagination";
+
+const Layout = ({ data, children }) => {
+  return (
+    <div className="row">
+      <div className="col col-md-3 mb-3">{data.groups}</div>
+      <div className="col">
+        {data.buttons}
+        <StatusMessage
+          item={data.item}
+          count={data.count}
+          group={data.group}
+          searchField={data.searchField}
+        >
+          {children}
+
+          <Pagination
+            totalPages={data.totalPages}
+            currentPage={data.currentPage}
+            onPageChange={data.onPageChange}
+          />
+        </StatusMessage>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
