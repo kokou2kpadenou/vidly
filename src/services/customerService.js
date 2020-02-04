167d1230cafd8085ac70a3afaa4cd56151159bd3
kokou kpadenou <kkokou@gmail.com>
2020-02-03 21:18:58 -0500

    customer service implemented

---
+++ src/services/customerService.js
@@ -0,0 +1,32 @@
+import http from "./httpService";
+
+const customersURL = "customers";
+
+function customerURL(customerId) {
+  return `${customersURL}/${customerId}`;
+}
+
+export function getCustomers() {
+  return http.get(customersURL);
+}
+
+export function deleteCustomer(customerId) {
+  return http.delete(customerURL(customerId));
+}
+
+export function getCustomer(customerId) {
+  return http.get(customerURL(customerId));
+}
+
+export function saveCustomer(customer) {
+  // put if exist
+  console.log(customer);
+
+  if (customer._id) {
+    const body = { ...customer };
+    delete body._id;
+    return http.put(customerURL(customer._id), body);
+  }
+  // push if not exist
+  return http.post(customersURL, customer);
+}
