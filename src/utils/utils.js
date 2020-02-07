a164a0be2b9f31f82cfb3d7e03d696c76629bb5c
kokou kpadenou <kkokou@gmail.com>
2020-02-07 17:18:16 -0500

    all utilities functions in one place

---
+++ src/utils/utils.js
@@ -0,0 +1,25 @@
+import _ from "lodash";
+
+export const dateFormating = date => {
+  return date ? new Date(date).toDateString().slice(4) : null;
+};
+
+export const feeUpToday = (dateOut, dailyRate) => {
+  const MILLISECONDS_PER_DAY = 1000 * 60 * 60 * 24;
+  const dateStart = new Date(dateOut);
+  const dateEnd = new Date();
+  const timeDiff = Math.abs(dateEnd.getTime() - dateStart.getTime());
+  const daysDiff = Math.ceil(timeDiff / MILLISECONDS_PER_DAY);
+  return dailyRate * daysDiff;
+};
+
+export const getDescendantProp = (obj, path) =>
+  path.split(".").reduce((acc, part) => acc && acc[part], obj);
+
+export function paginate(items, pageNumber, pageSize) {
+  const startIndex = (pageNumber - 1) * pageSize;
+  return _(items)
+    .slice(startIndex)
+    .take(pageSize)
+    .value();
+}
