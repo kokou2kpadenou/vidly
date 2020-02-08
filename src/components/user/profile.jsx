b3877cf0bf99a532f81a895e2df73f8f81dff34e
kokou kpadenou <kkokou@gmail.com>
2020-02-07 22:22:36 -0500

    add some details to profile

--- src/components/user/profile.jsx
+++ src/components/user/profile.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
+import auth from "../../services/authService";
+import SimpleInput from "../common/simpleInput";
 
-const Profile = () => {
-  return <h2>Profile</h2>;
-};
+class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {}
+    };
+  }
+
+  componentDidMount() {
+    const user = auth.getCurrentUser();
+
+    this.setState({ user });
+  }
+
+  // handleOnChange = (e) => {
+  //   this.setState()
+  // }
+
+  render() {
+    return (
+      <div>
+        <h2>Profile</h2>
+        <SimpleInput
+          value={this.state.user.name}
+          label="Name"
+          visibleLabel={true}
+        />
+        <SimpleInput
+          value={this.state.user.email}
+          label="Email"
+          visibleLabel={true}
+        />
+      </div>
+    );
+  }
+}
 
 export default Profile;
