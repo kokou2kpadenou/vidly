c41c83e5b8e73030fb54ac78164c2d789756c145
kokou kpadenou <kkokou@gmail.com>
2020-02-08 15:12:05 -0500

    error boundary component

---
+++ src/components/common/errorBoundary.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFormError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // log the  error to error service
+  }
+  render() {
+    if (this.state.hasError) {
+      return <div>Something went wrong.</div>;
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
