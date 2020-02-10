8caf46ff3492aebdfad955946016dee4a83f48cc
kokou kpadenou <kkokou@gmail.com>
2020-02-09 20:22:13 -0500

    collapse the navbar when item menu click

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
 
+const MenuLink = ({ to, label }) => (
+  <span data-toggle="collapse" data-target="#navbarNav">
+    <NavLink className="nav-item nav-link" to={to}>
+      {label}
+    </NavLink>
+  </span>
+);
+
 const NavBar = ({ user }) => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
@@ -23,37 +31,19 @@ const NavBar = ({ user }) => {
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
         <div className="navbar-nav">
-          <NavLink className="nav-item nav-link" to="/movies">
-            Movies
-          </NavLink>
-
-          <NavLink className="nav-item nav-link" to="/customers">
-            Customers
-          </NavLink>
-
-          <NavLink className="nav-item nav-link" to="/rentals">
-            Rentals
-          </NavLink>
+          <MenuLink to="/movies" label="Movies" />
+          <MenuLink to="/customers" label="Customers" />
+          <MenuLink to="/rentals" label="Rentals" />
           {!user && (
             <React.Fragment>
-              <NavLink className="nav-item nav-link" to="/login">
-                Login
-              </NavLink>
-
-              <NavLink className="nav-item nav-link" to="/register">
-                Register
-              </NavLink>
+              <MenuLink to="/login" label="Login" />
+              <MenuLink to="/register" label="Register" />
             </React.Fragment>
           )}
           {user && (
             <React.Fragment>
-              <NavLink className="nav-item nav-link" to="/profile">
-                {user.name}
-              </NavLink>
-
-              <NavLink className="nav-item nav-link" to="/logout">
-                Logout
-              </NavLink>
+              <MenuLink to="/profile" label={user.name} />
+              <MenuLink to="/logout" label="Logout" />
             </React.Fragment>
           )}
         </div>
