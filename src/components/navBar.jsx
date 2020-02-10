33f13afd1911877faf0924c4e9c098fff736fdfe
kokou kpadenou <kkokou@gmail.com>
2020-02-10 13:38:41 -0500

    collapse navbar only navbar in toggle mode

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,55 +1,67 @@
-import React from "react";
+import React, { Component } from "react";
 import { NavLink, Link } from "react-router-dom";
+import $ from "jquery";
 
-const MenuLink = ({ to, label }) => (
-  <span data-toggle="collapse" data-target="#navbarNav">
-    <NavLink className="nav-item nav-link" to={to}>
-      {label}
-    </NavLink>
-  </span>
-);
-
-const NavBar = ({ user }) => {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-      <Link className="navbar-brand" to="/">
-        <svg className="icon icon-vynil align-top mr-2">
-          <use xlinkHref="#icon-vynil" href="#icon-vynil" />
-        </svg>
-        Vidly
-      </Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
+class NavBar extends Component {
+  MenuLink = ({ to, label }) => {
+    return (
+      <span
+        onClick={() => {
+          if ($("#navbar-toggler").css("display") === "block") {
+            $("#navbarNav").collapse("toggle");
+          }
+        }}
       >
-        <span className="navbar-toggler-icon" />
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <div className="navbar-nav">
-          <MenuLink to="/movies" label="Movies" />
-          <MenuLink to="/customers" label="Customers" />
-          <MenuLink to="/rentals" label="Rentals" />
-          {!user && (
-            <React.Fragment>
-              <MenuLink to="/login" label="Login" />
-              <MenuLink to="/register" label="Register" />
-            </React.Fragment>
-          )}
-          {user && (
-            <React.Fragment>
-              <MenuLink to="/profile" label={user.name} />
-              <MenuLink to="/logout" label="Logout" />
-            </React.Fragment>
-          )}
+        <NavLink className="nav-item nav-link" to={to}>
+          {label}
+        </NavLink>
+      </span>
+    );
+  };
+  render() {
+    const { user } = this.props;
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <Link className="navbar-brand" to="/">
+          <svg className="icon icon-vynil align-top mr-2">
+            <use xlinkHref="#icon-vynil" href="#icon-vynil" />
+          </svg>
+          Vidly
+        </Link>
+        <button
+          className="navbar-toggler"
+          id="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <div className="navbar-nav">
+            <this.MenuLink to="/movies" label="Movies" />
+            <this.MenuLink to="/customers" label="Customers" />
+            <this.MenuLink to="/rentals" label="Rentals" />
+            {!user && (
+              <React.Fragment>
+                <this.MenuLink to="/login" label="Login" />
+                <this.MenuLink to="/register" label="Register" />
+              </React.Fragment>
+            )}
+            {user && (
+              <React.Fragment>
+                <this.MenuLink to="/profile" label={user.name} />
+                <this.MenuLink to="/logout" label="Logout" />
+              </React.Fragment>
+            )}
+          </div>
         </div>
-      </div>
-    </nav>
-  );
-};
+      </nav>
+    );
+  }
+}
 
 export default NavBar;
