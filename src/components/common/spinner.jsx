1e969643a3ad7b237ed81789f17723dceb82eaa2
kokou kpadenou <kkokou@gmail.com>
2020-04-11 21:39:04 -0400

    restyled spinner component

--- src/components/common/spinner.jsx
+++ src/components/common/spinner.jsx
@@ -5,26 +5,13 @@ import Loader from "react-loader-spinner";
 const Spinner = ({ area }) => {
   const { promiseInProgress } = usePromiseTracker({
     area: area,
-    delay: 500
+    delay: 500,
   });
-
-  const spinnerStyle = {
-    position: "fixed",
-    left: "0",
-    right: "0",
-    top: "0",
-    bottom: "0",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#fff",
-    Zindex: "1050"
-  };
 
   return (
     <>
       {promiseInProgress && (
-        <div style={spinnerStyle}>
+        <div className="spinner position-absolute d-flex justify-content-center align-items-center bg-white w-100 h-100">
           <Loader type="ThreeDots" color="#ff0000" height={100} width={100} />
         </div>
       )}
